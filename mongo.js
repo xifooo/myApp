2e8b46059b55087616496fef25a5bfa2534525fd
xifooo <82203432+xifooo@users.noreply.github.com>
2023-01-23 17:09:26 +0800

    Feature (#5)
    * 添加了mongoDB的支持
    
    * 后端：模块化数据库的连接，完善route Handler
    
    Co-authored-by: xifooo <xifooo@gmail.com>

--- mongo.js
+++ mongo.js
@@ -6,9 +6,7 @@ if (process.argv.length < 3) {
 };
 
 const password = process.argv[2];
-
 const url = `mongodb+srv://jyeho:${password}@cluster0.30kp0cf.mongodb.net/noteApp?retryWrites=true&w=majority`;
-
 mongoose.connect(url);
 
 const noteScehma = new mongoose.Schema({
@@ -16,19 +14,37 @@ const noteScehma = new mongoose.Schema({
     date: Date,
     important: Boolean,
 });
-
 const Note = mongoose.model("Note", noteScehma);
 
-const note = new Note({
-    content: "HTML is Easyyyy",
-    date: new Date(),
-    important: true,
-});
+// const note = new Note({
+//     content: "HTML is Easyyyy",
+//     date: new Date(),
+//     important: true,
+// });
 
-note.save().then(result => {
-    console.log('note saved!');
-    mongoose.connection.close();
-});
+// note.save().then(result => {
+//     console.log('note saved!');
+//     mongoose.connection.close();
+// });
+
+let notes = [
+    {
+        content: "HTML is easyyyyyyyy",
+        date: "2022-05-30T17:30:31.098Z",
+        important: true
+    },
+    {
+        content: "Browser can execute only Javascript",
+        date: "2022-05-30T18:39:34.091Z",
+        important: false
+    },
+    {
+        content: "GET and POST are the most important methods of HTTP protocol",
+        date: "2022-05-30T19:20:14.298Z",
+        important: true
+    }
+];
+Note.insertMany(notes, function(err) { });
 
 Note.find({}).then(result => {
     result.forEach(note => {
