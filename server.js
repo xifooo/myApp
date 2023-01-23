6e081dfa44c8b61954c77365f3d96316bc80fa7c
xifooo <xifooo@gmail.com>
2023-01-23 16:59:32 +0800

    后端：模块化数据库的连接，完善route Handler

--- server.js
+++ server.js
@@ -1,33 +1,15 @@
-let notes = [
-    {
-        id: 1,
-        content: "HTML is easyyyyyyyy",
-        date: "2022-05-30T17:30:31.098Z",
-        important: true
-    },
-    {
-        id: 2,
-        content: "Browser can execute only Javascript",
-        date: "2022-05-30T18:39:34.091Z",
-        important: false
-    },
-    {
-        id: 3,
-        content: "GET and POST are the most important methods of HTTP protocol",
-        date: "2022-05-30T19:20:14.298Z",
-        important: true
-    }
-];
-
+require('dotenv').config();
 const express = require("express");
+const cors = require('cors');
+const morgan = require("morgan");
+const path = require("path")
+const Note = require("./models/note")
+
 const app = express();
 app.use(express.json());
-app.use(express.static("build"))
+app.use(express.static(path.join(__dirname, "client", "build")));
 
-const cors = require('cors');
 app.use(cors());
-
-var morgan = require("morgan");
 // app.use(morgan("tiny"));
 app.use(morgan(function (tokens, request, response) {
     return [
@@ -39,62 +21,50 @@ app.use(morgan(function (tokens, request, response) {
     ].join('  ')
 }));
 
+
 app.get("/", (request, response) => {
     response.send("<h1>Hello World!</h1>")
 });
 
 app.get("/api/notes", (request, response) => {
-    response.json(notes);
+    Note.find({}).then(notes => {
+        response.json(notes);
+    });
 });
 
 app.get("/api/notes/:id", (request, response) => {
     const id = Number(request.params.id);
-    const note = notes.find(note => {
-        console.log(note.id,typeof note.id, id, typeof id, note.id === id)
-        return note.id ===id
-    });
-
-    if (note) {
+    Note.findOne({"id":id}).then(note => {
         response.json(note);
-    } else {
-        response.status(404).end();
-    };
+    });
 });
 
 app.delete("/api/notes/:id", (request, response) => {
     const id = Number(request.params.id);
-    notes = notes.filter(note => note.id !== id);
-
-    response.status(204).end();
+    Note.deleteOne({"id":id}).then(result => {
+        response.status(204).end();
+    });
+    // notes = notes.filter(note => note.id !== id);
+    // response.status(204).end();
 });
-
-const generateId = () => {
-    // 这种方法实际上是不推荐的, 我们很快就会取代它
-    const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0;
-    return maxId + 1
-};
 
 app.post("/api/notes", (request, response) => {
     const body = request.body;
 
-    if (!body.content) {
+    if (body.content === undefined) {
         return response.status(400).json({  // 400 Bad request
             error: "content missing"
         })
     };
-
-    const note = {
+    const note = new Note({
         content: body.content,
         important: body.important || false,
-        date: new Date(),
-        id: generateId(),
-    };
+        date: new Date()
+    });
 
-    notes = notes.concat(note);
-
-    response.json(note);
+    note.save().then(result => {
+        response.json(note);
+    });
 });
 
 const unknownEndpoint = (request, response) => {
@@ -102,7 +72,8 @@ const unknownEndpoint = (request, response) => {
 };
 app.use(unknownEndpoint);
 
-const PORT = 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 });
+
