b9d05017cec67691f10e78c1ec52da3439f53bf7
xifooo <82203432+xifooo@users.noreply.github.com>
2023-01-24 00:33:30 +0800

    Feature (#6)
    * 添加了mongoDB的支持
    
    * 后端：模块化数据库的连接，完善route Handler
    
    * 后端：已完整
    
    Co-authored-by: xifooo <xifooo@gmail.com>

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
-require('dotenv').config();
 const express = require("express");
-const cors = require('cors');
-const morgan = require("morgan");
-const path = require("path")
-const Note = require("./models/note")
-
 const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const morgan = require("morgan");
+const Note = require("./models/note");
+const path = require("path");
+
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "client", "build")));
 
 app.use(cors());
-// app.use(morgan("tiny"));
+
 app.use(morgan(function (tokens, request, response) {
     return [
         tokens.method(request, response),
@@ -21,7 +20,9 @@ app.use(morgan(function (tokens, request, response) {
     ].join('  ')
 }));
 
+app.use(express.static(path.join(".", "client", "build")));
 
+// route handlers
 app.get("/", (request, response) => {
     response.send("<h1>Hello World!</h1>")
 });
@@ -33,24 +34,28 @@ app.get("/api/notes", (request, response) => {
 });
 
 app.get("/api/notes/:id", (request, response) => {
-    const id = Number(request.params.id);
-    Note.findOne({"id":id}).then(note => {
-        response.json(note);
+    Note.findById(request.params.id)
+    .then(note => {
+        if (note) {
+                response.json(note);
+            } else {
+                response.status(404).end();
+            };
+        })
+        .catch(error => next(error))
     });
-});
-
+    
 app.delete("/api/notes/:id", (request, response) => {
-    const id = Number(request.params.id);
-    Note.deleteOne({"id":id}).then(result => {
-        response.status(204).end();
-    });
-    // notes = notes.filter(note => note.id !== id);
-    // response.status(204).end();
+    Note.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end();
+        })
+        .catch(error => next(error));
 });
 
-app.post("/api/notes", (request, response) => {
+app.post("/api/notes", (request, response, next) => {
     const body = request.body;
-
+    
     if (body.content === undefined) {
         return response.status(400).json({  // 400 Bad request
             error: "content missing"
@@ -61,19 +66,44 @@ app.post("/api/notes", (request, response) => {
         important: body.important || false,
         date: new Date()
     });
-
+    
     note.save().then(result => {
         response.json(note);
     });
 });
 
+app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body;
+  
+    const note = {
+      content: body.content,
+      important: body.important,
+    }
+  
+    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+      .then(updatedNote => {
+        response.json(updatedNote)
+      })
+      .catch(error => next(error))
+  })
+
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
 };
+
 app.use(unknownEndpoint);
 
-const PORT = process.env.PORT;
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+    if (error.name === "CastError") {
+        return response.status(400).send({ error: "malformatted id" })
+    }
+    next(error)
+};
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 });
-
