7acdc101a6a3b77f5b5a3e23909c6d133251e62b
xifooo <82203432+xifooo@users.noreply.github.com>
2023-01-25 22:49:37 +0800

    Feature (#7)
    * 添加了mongoDB的支持
    
    * 后端：模块化数据库的连接，完善route Handler
    
    * 后端：已完整
    
    * 后端：添加、使用了mongoose的验证器，但是前端的error提示有待改进
    
    Co-authored-by: xifooo <xifooo@gmail.com>

--- server.js
+++ server.js
@@ -69,23 +69,23 @@ app.post("/api/notes", (request, response, next) => {
     
     note.save().then(result => {
         response.json(note);
-    });
+    })
+    .catch(error => next(error));
 });
 
 app.put('/api/notes/:id', (request, response, next) => {
-    const body = request.body;
-  
-    const note = {
-      content: body.content,
-      important: body.important,
-    }
-  
-    Note.findByIdAndUpdate(request.params.id, note, { new: true })
-      .then(updatedNote => {
-        response.json(updatedNote)
-      })
-      .catch(error => next(error))
-  })
+  const { content, important } = request.body;
+
+  Note.findByIdAndUpdate(
+    request.params.id, 
+    { content, important }, 
+    { new: true, runValidators: true, context: "query" }
+    )
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
 
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
@@ -95,10 +95,14 @@ app.use(unknownEndpoint);
 
 const errorHandler = (error, request, response, next) => {
     console.error(error.message)
+
     if (error.name === "CastError") {
         return response.status(400).send({ error: "malformatted id" })
-    }
-    next(error)
+    } else if (error.name === "ValidationError") {
+        return response.status(400).json({ error: error.message})
+    };
+
+    next(error);
 };
 
 app.use(errorHandler);
