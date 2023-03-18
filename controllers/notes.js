5665058457fe2b1ce3c76a7177e275487fe0ec7b
xifooo <xifooo@tk2.com>
2023-03-18 11:11:02 +0800

    添加了后端应用的测试、异步async/await路由。

--- controllers/notes.js
+++ controllers/notes.js
@@ -2,64 +2,94 @@ const notesRouter = require('express').Router()
 const Note = require('../models/note')
 
 
-notesRouter.get("/", (request, response, next) => {
-  Note.find({}).then(notes => {
-      response.json(notes);
-  });
-});
+// notesRouter.get("/", (request, response, next) => {
+//     Note.find({}).then(notes => {
+//           response.json(notes);
+//   });
+// });
 
-notesRouter.get("/:id", (request, response, next) => {
-  Note.findById(request.params.id)
-  .then(note => {
-      if (note) {
-              response.json(note);
-          } else {
-              response.status(404).end();
-          };
-      })
-      .catch(error => next(error))
-  });
+// notesRouter.get('/:id', async (request, response, next) => {
+//   try {
+//     const note = await Note.findById(request.params.id)
+//     if (note) {
+//         response.json(note)
+//       } else {
+//       response.status(404).end()
+//     }
+//   } catch(exception) {
+//     next(exception)
+//     }
+// })
   
-notesRouter.delete("/:id", (request, response, next) => {
-  Note.findByIdAndRemove(request.params.id)
-      .then(result => {
-          response.status(204).end();
-      })
-      .catch(error => next(error));
-});
+notesRouter.get('/', async (request, response) => {
+  const notes = await Note.find({})
+  response.json(notes)
+})
 
-notesRouter.post("/", (request, response, next) => {
-  const body = request.body;
-  
-  if (body.content === undefined) {
-      return response.status(400).json({  // 400 Bad request
-          error: "content missing"
-      })
-  };
+notesRouter.get('/:id', async (request, response) => {
+  const note = await Note.findById(request.params.id)
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+})
+
+notesRouter.delete('/:id', async (request, response) => {
+  // 不再使用next(exception)，因为使用了express-async-erros库
+  // try {
+  //   await Note.findByIdAndRemove(request.params.id)
+  //   response.status(204).end()
+  // } catch (exception) {
+  //   next(exception)
+  // }
+    await Note.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+})
+
+notesRouter.post('/', async (request, response) => {
+  const body = request.body
+
   const note = new Note({
-      content: body.content,
-      important: body.important || false,
-      date: new Date()
-  });
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+  })
+
+  const savedNote = await note.save()
+  response.json(savedNote)
+})
+
+
+// notesRouter.post("/", (request, response, next) => {
+//   const body = request.body;
   
-  note.save().then(result => {
-      response.json(note);
-  })
-  .catch(error => next(error));
-});
+//   if (body.content === undefined) {
+//       return response.status(400).json({  // 400 Bad request
+//           error: "content missing"
+//       })
+//   };
+//   const note = new Note({
+//       content: body.content,
+//       important: body.important || false,
+//       date: new Date()
+//   });
+  
+//   note.save().then(savedNote => {
+//       response.status(201).json(savedNote);
+//   })
+//   .catch(error => next(error));
+// });
 
-notesRouter.put('/:id', (request, response, next) => {
-const { content, important } = request.body;
+notesRouter.put('/:id', async (request, response) => {
+  const { content, important } = request.body;
 
-Note.findByIdAndUpdate(
-  request.params.id, 
-  { content, important }, 
-  { new: true, runValidators: true, context: "query" }
-  )
-  .then(updatedNote => {
-    response.json(updatedNote)
-  })
-  .catch(error => next(error))
+  const updatedNote = await Note.findByIdAndUpdate(
+    request.params.id, 
+    { content, important }, 
+    { new: true, runValidators: true, context: "query" }
+    )
+  response.json(updatedNote)
 })
 
 module.exports = notesRouter
--- utils/logger.js
+++ utils/logger.js
@@ -1,10 +1,14 @@
 // info用于打印正常的日志信息，error用于所有的错误信息。
 const info = (...params) => {
-  console.log(...params)
+  if (process.env.NODE_ENV !== 'test') {
+    console.log(...params)
+  }
 }
 
 const error = (...params) => {
-  console.error(...params)
+  if (process.env.NODE_ENV !== 'test') {
+    console.error(...params)
+  }
 }
 
 
