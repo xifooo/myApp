49314f6c036cd39f7d03b4cef5e30e0a47489497
xifooo <82203432+xifooo@users.noreply.github.com>
2023-06-01 16:19:01 +0800

    bakup (#13)
    Co-authored-by: xifooo <xifooo@tk2.com>

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ const Footer = () => {
   return (
     <div style={footerStyle}>
       <br />
-      <em>Note app, Department of Computer Science, University of Helsinki 2022</em>
+      <em> Note app, Department of Computer Science, University of Helsinki 2022 </em>
     </div>
   )
 }
